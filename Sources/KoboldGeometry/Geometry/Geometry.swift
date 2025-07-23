a82c428985abf2400420bc7f45229023bdefc65d
Andy Stanton <andystanton@users.noreply.github.com>
2025-07-23 21:34:03 +0100

    Make init public

--- Sources/KoboldGeometry/Geometry/Geometry.swift
+++ Sources/KoboldGeometry/Geometry/Geometry.swift
@@ -1,11 +1,12 @@
 public struct KGEOGeometry {
     public let vertices: [(position: SIMD3<Float>, normal: SIMD3<Float>, texCoord: SIMD2<Float>?)]
     public let indices: [UInt32]
-}
 
-extension KGEOGeometry {
-    init(vertices: [(position: SIMD3<Float>, normal: SIMD3<Float>, texCoord: SIMD2<Float>?)]) {
+    public init(
+        vertices: [(position: SIMD3<Float>, normal: SIMD3<Float>, texCoord: SIMD2<Float>?)],
+        indices: [UInt32] = []
+    ) {
         self.vertices = vertices
-        self.indices = []
+        self.indices = indices
     }
 }
